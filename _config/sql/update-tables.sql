36bb88f1c8b46035a739fb997b34467b323af981
DaxPay Dev <dev@daxpay.com>
2026-06-16 11:22:40 +0800

    清理: 删除抖音表SQL, 清空菜单和更新SQL文件

--- _config/sql/update-tables.sql
+++
@@ -1,7 +0,0 @@
--- 支付宝直连商户应用增加应用类型字段
-ALTER TABLE "public"."alipay_direct_app"
-  ADD COLUMN "app_type" varchar(32);
-
--- 抖音直连商户应用授权认证配置增加应用密钥字段
-ALTER TABLE "public"."douyin_direct_app_auth_config"
-  ADD COLUMN "app_secret" varchar(512);
