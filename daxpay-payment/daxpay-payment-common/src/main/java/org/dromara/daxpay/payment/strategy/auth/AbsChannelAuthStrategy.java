4bd3d2c1c798408a380735c8f4625e0778cb6e72
DaxPay Dev <dev@daxpay.com>
2026-06-16 16:31:29 +0800

    feat(strategy): 迁移策略抽象类到 payment/strategy/
    - 新建 strategy/ 模块 (payment.strategy.{sub})
    - 8 个子域: pay/refund/transfer/sync/reconcile/profit/product/auth
    - 12 个 Abs* 策略文件从 old/pay/strategy 迁移完成

---
+++ daxpay-payment/daxpay-payment-common/src/main/java/org/dromara/daxpay/payment/strategy/auth/AbsChannelAuthStrategy.java
@@ -0,0 +1,24 @@
+package org.dromara.daxpay.payment.strategy.auth;
+
+import org.dromara.daxpay.payment.common.strategy.PaymentStrategy;
+import org.dromara.daxpay.payment.unipay.param.assist.AuthCodeParam;
+import org.dromara.daxpay.payment.unipay.param.assist.GenerateAuthUrlParam;
+import org.dromara.daxpay.payment.unipay.result.assist.AuthResult;
+import org.dromara.daxpay.payment.unipay.result.assist.AuthUrlResult;
+
+/// # 通道抽象认证策略
+///
+public abstract class AbsChannelAuthStrategy implements PaymentStrategy {
+
+    /// 获取授权链接
+    public abstract AuthUrlResult generateAuthUrl(GenerateAuthUrlParam param);
+
+    /// 通过AuthCode获取认证结果
+    public abstract AuthResult doAuth(AuthCodeParam param);
+
+    /// 获取微信OpenId认证是否使用道通认证方式
+    public boolean isWxChannelAuth() {
+        return false;
+    }
+
+}
---
+++ daxpay-payment/daxpay-payment-common/src/main/java/org/dromara/daxpay/payment/strategy/sync/AbsSyncTransferOrderStrategy.java
@@ -0,0 +1,21 @@
+package org.dromara.daxpay.payment.strategy.sync;
+
+import org.dromara.daxpay.payment.common.strategy.PaymentStrategy;
+import org.dromara.daxpay.payment.old.pay.bo.sync.TransferSyncResultBo;
+import org.dromara.daxpay.payment.old.pay.entity.order.transfer.TransferOrder;
+import lombok.Getter;
+import lombok.Setter;
+
+/// # 转账同步策略
+///
+@Getter
+@Setter
+public abstract class AbsSyncTransferOrderStrategy implements PaymentStrategy {
+
+    private TransferOrder transferOrder;
+
+    /// 同步前处理, 主要是预防请求过于迅速, 支付网关没有处理完退款请求, 导致返回的状态不正确
+    public void doBeforeHandler(){}
+    /// 查询通道网关方的退款订单状态信息
+    public abstract TransferSyncResultBo doSync();
+}
